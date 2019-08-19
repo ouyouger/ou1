9e8392f0dc8dad17ee032a251328ab73dfc8048d
ou <xxx@163.com>
2019-08-19 10:27:02 +0800

    Create 工具库.js
    做了个随机获取RGB类型颜色的函数

---
+++ 工具库.js
@@ -0,0 +1,8 @@
+
+//随机获取RGB类型的颜色
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
