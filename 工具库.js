e9e98168efc2b4fb57cff484d00731b3af2cccc3
ou <xxx@163.com>
2019-08-19 10:50:11 +0800

    Update 工具库.js
    做了时间获取的函数和获取m-n之间的随机整数

--- 工具库.js
+++ 工具库.js
@@ -6,3 +6,28 @@ function randomColor() {
     var b = Math.floor(Math.random() * 256);
     return "rgb(" + r + "," + g + "," + b + ")";
 }
+//获取当前时间
+function getFormateDate() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    month = patchFrontZero(month);
+    var day = date.getDate();
+    day = patchFrontZero(day);
+    var hour = date.getHours();
+    hour = patchFrontZero(hour);
+    var minutes = date.getMinutes();
+    minutes = patchFrontZero(minutes);
+    var seconds = date.getSeconds();
+    seconds = patchFrontZero(seconds);
+    var format = year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
+    return format;
+}
+//获取n-m之间的整数
+function rnd(n, m) {
+    var random = Math.floor(Math.random() * (m - n + 1) + n);
+    return random;
+}
+
+
+
